df98171c5f337731ddfa23cfe6778743663a8918
Ashley <Ashhamilton22888@gmail.com>
2023-04-25 14:25:42 -0400

    mern infrastructure boilerplate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import NewOrderPage from './pages/NewOrderPage';
+import OrderHistoryPage from './pages/OrderHistoryPage';
+import AuthPage from './pages/AuthPage';
 import './App.css';
+import NavBar from './components/NavBar';
+import { getUser } from "./utilities/users-service"
+
+
+
 
 function App() {
+  const [user, setUser] = useState(getUser())
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      {/* if user is true show new order page, else show authpage */}
+     { user ? 
+     <>
+     <NavBar user={user} setUser={setUser} />
+     <Routes>
+        <Route path='/orders/new' element={ <NewOrderPage /> } />
+        <Route path='/orders' element= {<OrderHistoryPage />} />
+     </Routes>
+     </>
+     : 
+     <AuthPage setUser={setUser}/>
+     }
+    </main>
   );
 }
 
